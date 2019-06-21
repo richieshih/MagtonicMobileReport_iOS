b221c6619e1e1c6aeb6c372f24b0ee9c4a2718a0
richie shih <kotzen1023@gmail.com>
2019-06-21 11:34:10 +0800

    Initial Commit

---
+++ MagtonicMobileReport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagtonicMobileReport
+//
+//  Created by richie shih on 2019/6/21.
+//  Copyright © 2019 richie shih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
